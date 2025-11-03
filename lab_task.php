a00b09f22c296afa0b3a30867983900e0188d2d2
Tazim Ahamed <mtazim.j@gmail.com>
2025-11-04 00:05:04 +0600

    My first code

---
+++ lab_task.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+    <title></title>
+
+</head>
+<body>
+    <center>
+        <h1 style="color:blue">Clinic Patient Registation</h1>
+    </center>
+</body>
+<center>
+   
+
+    
+    <from>
+        
+         <tr>
+       <td><h4> Full Name:</h4></td> 
+        
+        <td><input type="text"></td>
+         </tr>
+       </from>
+   <tr>
+
+   
+   <td> <h4> Age:</h4></td>
+        
+        <td><input type="text"></td> 
+        </tr>
+         <tr>
+        <td><h4> Phone Number:</h4></td> 
+        
+        <td><input type="number"></td>
+        </tr>
+        <tr>
+        <td><h4> Email Address:</h4></td>
+        
+      
+<input type="text">
+       
+        <br>
+        <br>
+        <tr>
+
+        
+        <td><b>Inasurance Provider:</b></td>
+        <br>
+        
+        <select>
+<option> Select Provider</option>
+<option> CBL</option>
+<option>DBL</option>
+
+        </select>
+ 
+        <h4> Insurance Policy Number:</h4>
+        
+        <input type="number">
+        <br>      
+        <h2 style ="color:blue"> Additional Information</h2>
+        
+        <h4> Username:</h4>
+        
+        <input type="text">
+        
+        <h4> password:</h4>
+        
+        <input type="text">
+       
+        <h4> Confirm password:</h4>
+        
+        <input type="text">
+        <br>
+        <br>
+
+        <button style="color:brown"> Registor</button>
+
+        
+    </from>
+</center>
